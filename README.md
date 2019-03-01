79e427bd2fca9b157bcc6545249988ce19da8fc5
Bing Ren <bingtimren@gmail.com>
2019-03-01 15:53:39 +1100

    V1.1 - provides batch loop function to handle unprocessed items from batch write

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # dynamodb-commons
 A collection of small node/AWS dynamodb utilities
+
+## Build and release
+
+To build, simply `tsc`
+To release, first `npm login`, then check version and `npm publish`
+
+## Usage
+
+`npm install dynamodb-commons`
+
+
---
+++ batch-loop.ts
@@ -0,0 +1,45 @@
+// import entire SDK
+import AWS = require('aws-sdk');
+
+/**
+ * Do batchwrite in a loop until no unprocessed items left
+ * @param dbDocClient - AWS.DynamoDB.DocumentClient
+ * @param params - AWS.DynamoDB.DocumentClient.BatchWriteItemInput
+ * @param maxLoop - maximum number of loops
+ * @param minDelay - minimum delay
+ * @param maxDelay - maximum delay
+ * @returns response, either (1) Without unprocessed, signaling finish, or 
+ *            (2) WITH unprocessed, done maximum loops 
+ */
+export async function dbBatchWriteLoop (
+    dbDocClient:AWS.DynamoDB.DocumentClient, 
+    params:AWS.DynamoDB.DocumentClient.BatchWriteItemInput, 
+    maxLoop:number  = 20, 
+    minDelay:number =500, 
+    maxDelay:number =2500) : Promise<AWS.DynamoDB.DocumentClient.BatchWriteItemOutput|undefined>
+    {
+    let batchWriteResp: AWS.DynamoDB.DocumentClient.BatchWriteItemOutput|undefined = undefined
+    for (var i=0; i<maxLoop; i++){
+        // error will be thrown
+        try {
+            batchWriteResp = await dbDocClient.batchWrite(params).promise()
+        let unprocessed = batchWriteResp.UnprocessedItems
+        // return, if no more unprocessed items, or this is the last loop
+        if ((unprocessed===undefined || Object.keys(unprocessed).length===0) || (i+1===maxLoop)) {
+            return batchWriteResp
+        }
+        // prepare to retry
+        params = {RequestItems:unprocessed}
+        // delay a random time 
+        const delay = Math.floor(Math.random() * (maxDelay - minDelay) + minDelay)
+        await new Promise(resolve => setTimeout(resolve, delay));
+        } catch (error){
+            throw {
+                error: error,
+                lastBatchWriteResponse : batchWriteResp
+            } 
+        }
+    }
+}
+
+
